5e51a876a228ac96bd0dc0015f8e555f389528dd
Josh Thomas <josh@ionic.io>
2016-06-03 11:19:26 -0500

    Add commented out syntax of sub folder usage.

--- dependency-with-es2015-and-commonjs/dist/bundle.rollup.js
+++ dependency-with-es2015-and-commonjs/dist/bundle.rollup.js
@@ -2,4 +2,6 @@ function cube ( x ) {
   return x * x * x;
 }
 
+// import {cube} from 'poor-math-skills/ecm';
+
 console.log(cube(5));
--- dependency-with-es2015-and-commonjs/src/index.js
+++ dependency-with-es2015-and-commonjs/src/index.js
@@ -1,3 +1,4 @@
 import {cube} from 'poor-math-skills';
+// import {cube} from 'poor-math-skills/ecm';
 
 console.log(cube(5));
