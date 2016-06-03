98d16e46e7f5db263f69e8c76ec8fc2f9af19ca1
Josh Thomas <josh@ionic.io>
2016-06-02 19:08:58 -0500

    Clarify imports in a comment.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
+/*
+ * Uncomment to test in package module tree-shaking
+ */
 // import {cube} from './poor-math-skills';
+
+/*
+ * Uncomment to test NPM based module tree-shaking
+ */
 import {cube} from 'poor-math-skills';
 
 console.log(cube(5));
