45b7d72625e2f72a5b70c1f4d045e6f281e50de8
Josh Thomas <josh@ionic.io>
2016-06-02 19:07:05 -0500

    Updated readme and scenario.

---
+++ README.md
@@ -0,0 +1,4 @@
+### testing tree-shaking with rollup and webpack2
+
+`npm run webpack`
+`npm run rollup`
--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
-import {cube} from './poor-math-skills';
+// import {cube} from './poor-math-skills';
+import {cube} from 'poor-math-skills';
 
 console.log(cube(5));
